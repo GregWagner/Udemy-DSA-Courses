161f2e605921bec1bb6d524344e66ac855b4de4a
Greg Wagner <370026+GregWagner@users.noreply.github.com>
2023-01-17 11:42:36 -0600

    Restarting (again)

--- Mastering_Critical_Skills/Skills/Section_04_Data_Types_and_Variables/Hard/Section_04_HW_07_Print_Me.cpp
+++ Mastering_Critical_Skills/Skills/Section_04_Data_Types_and_Variables/Hard/Section_04_HW_07_Print_Me.cpp
@@ -2,6 +2,7 @@
  * Print Me
  * if b is -1, print 2 * a + 1
  * if b is 1, print a * a
+ *
  * Hint - you need to find a simple 1 line formula
  * Need to convert b to 0 or 1
  */
@@ -24,6 +25,7 @@ auto main() -> int
     // b = 1: b_is_1 = 1
     // b = -1: b_is_1 = 0
     int b_is_1 = (b + 1) / 2;
+
     // b = 1: b_is_neg_1 = 0
     // b = -1: b_is_neg_1 = 1
     int b_is_neg_1{1 - b_is_1};
@@ -31,5 +33,5 @@ auto main() -> int
     // Either 1 * equation_1 + 0 * equation_2
     // or
     //        0 * equation_1 + 1 * equation_2
-    std::cout << b_is_1 * if_b_is_one + b_is_neg_1 * if_b_is_minus_one << '\n';
+    std::cout << (b_is_1 * if_b_is_one) + (b_is_neg_1 * if_b_is_minus_one) << '\n';
 }
