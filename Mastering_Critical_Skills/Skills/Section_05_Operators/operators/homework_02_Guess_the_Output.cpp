f0b298407fc5238d03afbf566c94978665d8b810
Greg Wagner <370026+GregWagner@users.noreply.github.com>
2023-04-09 20:21:43 -0500

    Started section 5

---
+++ Mastering_Critical_Skills/Skills/Section_05_Operators/operators/homework_02_Guess_the_Output.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+auto main() -> int {
+    int c {};
+    int b { 1 };
+    int a { 1 };
+
+    std::cout << (c = a + b, a = b, b = c,
+        c = a + b, a = b, b = c,
+        c = a + b, a = b, b = c,
+        c = a + b, a = b, b = c)
+              << '\n';
+    std::cout << a << ' ' << b << ' ' << c << '\n';
+}
+
+/*
+    c = 1 + 1 = 2,
+    a = 1, b = 2, c = 2
+
+    c = 1 + 2 = 3
+    a = 2, b = 3, c = 3
+
+    c = 2 + 3 = 5,
+    a = 3, b = 5,
+
+    c = 3 + 5 = 8, a = 5, b = 8)
+    cout << 8 << '\n';
+*/
