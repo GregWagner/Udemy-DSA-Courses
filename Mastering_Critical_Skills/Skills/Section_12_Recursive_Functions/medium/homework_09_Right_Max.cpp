7e794a9d27f8898c33db4443db132b131d5432ec
Greg Wagner <370026+GregWagner@users.noreply.github.com>
2023-04-18 07:41:40 -0500

    Finished Section 12

--- Mastering_Critical_Skills/Skills/Section_12_Recursive_Functions/medium/homework_09_Right_Max.cpp
+++ Mastering_Critical_Skills/Skills/Section_12_Recursive_Functions/medium/homework_09_Right_Max.cpp
@@ -1,16 +1,18 @@
+// this is not correct
+
 #include <iostream>
 
-auto left_max(int arr[], int len) -> void {
+auto right_max(int arr[], int len, int start_position=0) -> void {
     if (len == 1) {
         return;
     }
-    left_max(arr, len - 1);
     arr[len - 1] = std::max(arr[len - 1], arr[len - 2]);
+    right_max(arr, len - 1, start_position + 1);
 }
 
 auto main() -> int {
     int a[] { 1, 3, 5, 7, 4, 2 };
-    left_max(a, 7);
+    right_max(a, 7);
     for (int i {}; i < 6; ++i) {
         std::cout << a[i] << ' ';
     }
